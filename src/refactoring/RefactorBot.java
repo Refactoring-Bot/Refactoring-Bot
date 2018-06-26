5204163d2ac0f7bbcd2cd0c4dadc5e5ab28708f7
Timo Pfaff <TimoPfaff88@gmx.de>
2018-06-26 23:38:48 +0200

    move logic from main method to other methods, add comments

--- src/refactoring/RefactorBot.java
+++ src/refactoring/RefactorBot.java
@@ -1,17 +1,7 @@
 package refactoring;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.apache.commons.exec.CommandLine;
-import org.apache.commons.exec.DefaultExecutor;
-import org.apache.commons.exec.ExecuteException;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -21,70 +11,56 @@ import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import com.github.javaparser.JavaParser;
-import com.github.javaparser.ast.CompilationUnit;
-
 public class RefactorBot {
-	
 
-	public static void main(String[] args) throws IOException, InterruptedException {
-		String path = "";
+	/**
+	 * Get needed Issues from Sonarqube and parse it to a JSON Array
+	 * 
+	 * @throws IOException
+	 */
+	public static JSONArray getSonarqubeIssues(String projectName) throws IOException {
+
 		// HttpGet httpGet = new HttpGet("http://localhost:9000/api/issues/search?resolved=false&format=json");
 		// HttpGet httpGet = new HttpGet("https://sonarcloud.io/api/issues/search?organization=timopfaff-github&resolved=false&format=json");
-		
-		/**
-		 * Get needed Issues from Sonarqube and parse it to a JSON Array
-		 * TODO: Replace hardcoded url(name of the project)
-		 */
 		HttpGet httpGet = new HttpGet(
-				"https://sonarcloud.io/api/issues/search?projects=Test:Test:master&resolved=false&format=json");
+				"https://sonarcloud.io/api/issues/search?projects=" + projectName + "&resolved=false&format=json");
 		try (CloseableHttpClient httpClient = HttpClients.createDefault();
 				CloseableHttpResponse response = httpClient.execute(httpGet);) {
 			HttpEntity entity = response.getEntity();
 			String json = EntityUtils.toString(entity);
 			JSONObject obj = new JSONObject(json);
 			System.out.println(obj);
-			JSONArray arr = obj.getJSONArray("issues");
-			
-			/**
-			 * The Refactoring itself
-			 */
-			for (int i = 0; i < arr.length(); i++) {
-				String rule = arr.getJSONObject(i).getString("rule");
-				if (rule.equals("squid:S1068")) {
-					String project = arr.getJSONObject(i).getString("project");
-					String component = arr.getJSONObject(i).getString("component");
-					path = component.substring(project.length() + 1, component.length());
-					String message = arr.getJSONObject(i).getString("message");
-					String name = StringUtils.substringBetween(message, "\"", "\"");
-					FileInputStream in = new FileInputStream("c://Users/Timo/Test/git/Calculator/" + path);
-					CompilationUnit compilationUnit = JavaParser.parse(in);
-					System.out.println(compilationUnit.toString());
-					VariableDeletor visitor = new VariableDeletor();
-					visitor.setVariableName(name);
-					visitor.RemoveUnusedVariable(compilationUnit);
-					System.out.println(compilationUnit.toString());
-					
-					
-					/**
-					 * Actually apply changes to the File
-					 * TODO: Replace hardcoded Path 
-					 */
-					 PrintWriter out = new PrintWriter("c://Users/Timo/Test/git/Calculator/" + path); 
-					 out.println(compilationUnit.toString());
-					 out.close();
-					 
-				}
-			}
-			 /**
-			  * execute script to commit changes and create pull request on GitHub
-			  */
-			 String bash = "c:/Programme/Git/bin/bash.exe"; String filename =
-			 "c:/Users/Timo/pull-request.sh"; String[] command = new String[] { bash,
-			 filename }; ProcessBuilder p = new ProcessBuilder(command).inheritIO();
-			 Process pb = p.start(); pb.waitFor();
-			 
-		}
+			return obj.getJSONArray("issues");
 
+		}
 	}
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+
+		JSONArray issues = getSonarqubeIssues("Test:Test:master");
+
+		/**
+		 * The Refactoring itself
+		 */
+		for (int i = 0; i < issues.length(); i++) {
+			String rule = issues.getJSONObject(i).getString("rule");
+			if (rule.equals("squid:S1068")) {
+				
+				VariableDeletor deletor = new VariableDeletor();
+				deletor.RemoveUnusedVariable(issues.getJSONObject(i), "c://Users/Timo/Test/git/Calculator/");
+
+			}
+		}
+		/**
+		 * execute script to commit changes and create pull request on GitHub
+		 */
+		String bash = "c:/Programme/Git/bin/bash.exe"; 
+		String filename = "c:/Users/Timo/pull-request.sh"; 
+		String[] command = new String[] { bash, filename }; 
+		ProcessBuilder p = new ProcessBuilder(command).inheritIO();
+		Process pb = p.start(); 
+		pb.waitFor();
+		
+	}
+
 }
--- src/refactoring/VariableDeletor.java
+++ src/refactoring/VariableDeletor.java
@@ -1,9 +1,19 @@
 package refactoring;
 
+import org.json.JSONObject;
+
+import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.VariableDeclarator;
 import com.github.javaparser.ast.visitor.ModifierVisitor;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * 
  * @author Timo Pfaff
@@ -24,8 +34,26 @@ public class VariableDeletor extends ModifierVisitor<Void> {
 
 	}
 	
-	public void RemoveUnusedVariable(CompilationUnit compilationUnit) {
+	public void RemoveUnusedVariable(JSONObject issue, String projectPath) throws FileNotFoundException {
+		String project = issue.getString("project");
+		String component = issue.getString("component");
+		String path = component.substring(project.length() + 1, component.length());
+		String message = issue.getString("message");
+		String name = StringUtils.substringBetween(message, "\"", "\"");
+		FileInputStream in = new FileInputStream(projectPath + path);
+		CompilationUnit compilationUnit = JavaParser.parse(in);
+		System.out.println(compilationUnit.toString());
+		this.setVariableName(name);
 		this.visit(compilationUnit, null);
+		System.out.println(compilationUnit.toString());
+		
+		/**
+		 * Actually apply changes to the File 
+		 */
+		 PrintWriter out = new PrintWriter(projectPath + path);
+		 out.println(compilationUnit.toString());
+		 out.close();
+
 	}
 
 	public String getVariableName() {
