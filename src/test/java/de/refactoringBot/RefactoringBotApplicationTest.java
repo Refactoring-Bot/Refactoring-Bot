893be2fdcd4ff22270b9914eb61f420fe24b07d1
Stefan Basaric <st111465@stud.uni-stuttgart.de>
2018-12-10 11:15:13 +0100

    Removed empty test

--- src/test/java/de/refactoringBot/RefactoringBotApplicationTest.java
+++ src/test/java/de/refactoringBot/RefactoringBotApplicationTest.java
@@ -1,6 +1,5 @@
 package de.refactoringBot;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -8,9 +7,4 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RefactoringBotApplicationTest {
-
-	@Test
-	public void contextLoads() {
-	}
-
 }
