a640566b6a7e528e66d39902b4c826495aa87a8b
Stefan Basaric <st111465@stud.uni-stuttgart.de>
2018-12-10 11:08:41 +0100

    Renamed Test-Package to match application package; added sonar plugin

--- src/test/java/de/refactoringBot/RefactoringBotApplicationTest.java
+++ src/test/java/de/refactoringBot/RefactoringBotApplicationTest.java
@@ -1,4 +1,4 @@
-package de.BA.gitHubApi;
+package de.refactoringBot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class GitHubApiTestApplicationTests {
+public class RefactoringBotApplicationTest {
 
 	@Test
 	public void contextLoads() {
