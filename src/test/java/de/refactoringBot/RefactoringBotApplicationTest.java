452909a71387ecae95549846a931abf4dabb585a
Stefan Basaric <st111465@stud.uni-stuttgart.de>
2018-12-10 11:19:49 +0100

    Removed Test-Annotations from unused Test-Class

--- src/test/java/de/refactoringBot/RefactoringBotApplicationTest.java
+++ src/test/java/de/refactoringBot/RefactoringBotApplicationTest.java
@@ -1,10 +1,6 @@
 package de.refactoringBot;
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
+// @RunWith(SpringRunner.class)
+// @SpringBootTest
 public class RefactoringBotApplicationTest {
 }
