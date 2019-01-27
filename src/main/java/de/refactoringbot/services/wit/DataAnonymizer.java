ffd1ee017b48850cc13165ebcf153b15da995344
Stefan Basaric <st111465@stud.uni-stuttgart.de>
2019-01-27 19:18:17 +0100

    1. version of token anonymization added

--- src/main/java/de/refactoringbot/services/wit/DataAnonymizer.java
+++ src/main/java/de/refactoringbot/services/wit/DataAnonymizer.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 public class DataAnonymizer {
 
 	public static final String URLEXP = "^(http://|https://)?(www.)?([a-zA-Z0-9]+).[a-zA-Z0-9]*.[a-z]{3}.([a-z]+)$";
+	public static final String TOKENEXP = "^[a-z0-9]{40}$";
 
 	/**
 	 * This method checks a comment if it contains any possible private data and
@@ -29,8 +30,43 @@ public class DataAnonymizer {
 		comment = anonymizeURLs(comment);
 		// Remove all tagged words (usually usernames)
 		comment = anonymizeTaggedUser(comment);
+		// Remove all words that could be access tokens
+		comment = anonymizePossibleTokens(comment);
 
 		return comment;
+	}
+
+	/**
+	 * This method translates all email-addresses from a comment to the keyword
+	 * 'TOKEN'.
+	 * 
+	 * @param comment
+	 * @return commentWithoutTokens
+	 */
+	private String anonymizePossibleTokens(String comment) {
+		// Init anonymized comment
+		String anonymizedComment = "";
+		// Split input comment into words
+		String[] words = comment.split(" ");
+
+		// Iterate all words
+		for (String word : words) {
+			// If word is some link
+			if (word.matches(TOKENEXP)) {
+				// Add URL Keyword to anonymized comment
+				anonymizedComment = anonymizedComment.concat("TOKEN ");
+			} else {
+				// Copy nun URL word to anonymized comment
+				anonymizedComment = anonymizedComment.concat(word + " ");
+			}
+		}
+
+		// Remove " " at end of comment that was created during anonymization
+		if (anonymizedComment.endsWith(" ")) {
+			anonymizedComment = anonymizedComment.substring(0, anonymizedComment.length() - 1);
+		}
+
+		return anonymizedComment;
 	}
 
 	/**
