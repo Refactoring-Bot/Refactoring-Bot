4157f6df3ff1ab380a104f61997f4ef46f35edee
Marvin Wyrich <mail@marvin-wyrich.de>
2019-03-20 13:57:14 +0100

    Split test method for RenameMethod into smaller tests for better extensibility

--- src/test/java/de/refactoringbot/refactorings/RenameMethodTest.java
+++ src/test/java/de/refactoringbot/refactorings/RenameMethodTest.java
@@ -3,10 +3,11 @@ package de.refactoringbot.refactorings;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 
-import org.assertj.core.api.SoftAssertions;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -41,137 +42,137 @@ public class RenameMethodTest extends AbstractRefactoringTests {
 	private TestDataSubClassRenameMethod renameMethodSubClass = new TestDataSubClassRenameMethod();
 	private TestDataSiblingClassRenameMethod renameMethodSiblingClass = new TestDataSiblingClassRenameMethod();
 
+	private File fileOfTestClass;
+	private File fileOfSuperClass;
+	private File fileOfSubClass;
+	private File fileWithCallerMethod;
+	private File fileOfSiblingClass;
+
 	@Rule
 	public final ExpectedException exception = ExpectedException.none();
 
-	@Test
-	public void testRenameMethod() throws Exception {
-		testRenameMethod(renameMethodTestClass.getLineOfMethodToBeRenamed(true), "changedMethodName");
+	@Before
+	public void createTempCopiesOfTestResourceFiles() throws IOException {
+		fileOfTestClass = createTempCopyOfTestResourcesFile(TestDataClassRenameMethod.class);
+		fileOfSuperClass = createTempCopyOfTestResourcesFile(TestDataSuperClassRenameMethod.class);
+		fileOfSubClass = createTempCopyOfTestResourcesFile(TestDataSubClassRenameMethod.class);
+		fileWithCallerMethod = createTempCopyOfTestResourcesFile(TestDataClassWithCallOfTargetMethod.class);
+		fileOfSiblingClass = createTempCopyOfTestResourcesFile(TestDataSiblingClassRenameMethod.class);
 	}
 
 	@Test
-	public void testRenameMethodUnchangedMethodName() throws Exception {
-		exception.expect(BotRefactoringException.class);
-		testRenameMethod(renameMethodTestClass.getLineOfMethodToBeRenamed(true), "getLineOfMethodToBeRenamed");
-	}
-
-	/**
-	 * Tries to rename the method in the given line to the given new method name.
-	 * Checks if refactoring was successful in all test data classes
-	 * 
-	 * @param lineNumberOfMethodToBeRenamed
-	 * @param newMethodName
-	 * @throws Exception
-	 */
-	private void testRenameMethod(int lineNumberOfMethodToBeRenamed, String newMethodName) throws Exception {
+	public void testTargetClassRefactored() throws Exception {
 		// arrange
-		File fileWithCodeSmell = createTempCopyOfTestResourcesFile(TestDataClassRenameMethod.class);
-		File fileOfSuperClass = createTempCopyOfTestResourcesFile(TestDataSuperClassRenameMethod.class);
-		File fileOfSubClass = createTempCopyOfTestResourcesFile(TestDataSubClassRenameMethod.class);
-		File fileWithCallerMethod = createTempCopyOfTestResourcesFile(TestDataClassWithCallOfTargetMethod.class);
-		File fileOfSiblingClass = createTempCopyOfTestResourcesFile(TestDataSiblingClassRenameMethod.class);
+		List<File> filesToConsider = new ArrayList<File>();
+		filesToConsider.add(fileOfTestClass);
+		int lineNumberOfMethodToBeRenamed = renameMethodTestClass.getLineOfMethodToBeRenamed(true);
+		String newMethodName = "newMethodName";
 
-		CompilationUnit cuOriginalFileWithCodeSmell = JavaParser.parse(fileWithCodeSmell);
-		CompilationUnit cuOriginalFileWithCallerMethod = JavaParser.parse(fileWithCallerMethod);
-		CompilationUnit cuOriginalFileOfSuperClass = JavaParser.parse(fileOfSuperClass);
-		CompilationUnit cuOriginalFileOfSubClass = JavaParser.parse(fileOfSubClass);
-		CompilationUnit cuOriginalFileOfSiblingClass = JavaParser.parse(fileOfSiblingClass);
-
-		MethodDeclaration originalMethod = RefactoringHelper
-				.getMethodDeclarationByLineNumber(lineNumberOfMethodToBeRenamed, cuOriginalFileWithCodeSmell);
+		CompilationUnit cuOriginalFileOfTestClass = JavaParser.parse(fileOfTestClass);
 		MethodDeclaration originalDummyMethod = RefactoringHelper.getMethodDeclarationByLineNumber(
-				renameMethodTestClass.getLineOfMethodToBeRenamed(), cuOriginalFileWithCodeSmell);
-		MethodDeclaration originalCallerMethod = RefactoringHelper.getMethodDeclarationByLineNumber(
-				renameMethodTestClass.getLineOfMethodThatCallsMethodToBeRenamed(), cuOriginalFileWithCodeSmell);
-		MethodDeclaration originalCallerMethodInnerClass = RefactoringHelper.getMethodDeclarationByLineNumber(
-				renameMethodInnerTestClass.getLineNumberOfCallerInInnerClass(), cuOriginalFileWithCodeSmell);
-		MethodDeclaration originalCallerMethodInDifferentFile = RefactoringHelper.getMethodDeclarationByLineNumber(
-				renameMethodCallerTestClass.getLineOfCallerMethodInDifferentFile(), cuOriginalFileWithCallerMethod);
-		MethodDeclaration originalMethodInSuperClass = RefactoringHelper.getMethodDeclarationByLineNumber(
-				renameMethodSuperClass.getLineOfMethodToBeRenamed(true), cuOriginalFileOfSuperClass);
-		MethodDeclaration originalMethodInSubClass = RefactoringHelper.getMethodDeclarationByLineNumber(
-				renameMethodSubClass.getLineOfMethodToBeRenamed(true), cuOriginalFileOfSubClass);
-		MethodDeclaration originalMethodInSiblingClass = RefactoringHelper.getMethodDeclarationByLineNumber(
-				renameMethodSiblingClass.getLineOfMethodToBeRenamed(true), cuOriginalFileOfSiblingClass);
-		MethodDeclaration originalCallerMethodInSiblingClass = RefactoringHelper.getMethodDeclarationByLineNumber(
-				renameMethodSiblingClass.getLineNumberOfCallerInSiblingClass(), cuOriginalFileOfSiblingClass);
+				renameMethodTestClass.getLineOfMethodToBeRenamed(), cuOriginalFileOfTestClass);
 		MethodDeclaration originalMethodWithTargetMethodSignatureInInnerClass = RefactoringHelper
 				.getMethodDeclarationByLineNumber(renameMethodInnerTestClass.getLineOfMethodToBeRenamed(true),
-						cuOriginalFileWithCodeSmell);
-
-		SoftAssertions softAssertions = new SoftAssertions();
-		softAssertions.assertThat(originalMethod).isNotNull();
-		softAssertions.assertThat(originalDummyMethod).isNotNull();
-		softAssertions.assertThat(originalCallerMethod).isNotNull();
-		softAssertions.assertThat(originalCallerMethodInnerClass).isNotNull();
-		softAssertions.assertThat(originalCallerMethodInDifferentFile).isNotNull();
-		softAssertions.assertThat(originalMethodInSuperClass).isNotNull();
-		softAssertions.assertThat(originalMethodInSubClass).isNotNull();
-		softAssertions.assertThat(originalMethodInSiblingClass).isNotNull();
-		softAssertions.assertThat(originalCallerMethodInSiblingClass).isNotNull();
-		softAssertions.assertThat(originalMethodWithTargetMethodSignatureInInnerClass).isNotNull();
-		softAssertions.assertAll();
-
-		GitConfiguration gitConfig = new GitConfiguration();
-		gitConfig.setRepoFolder(getAbsolutePathOfTempFolder());
-
-		ArrayList<String> javaRoots = new ArrayList<>();
-		javaRoots.add(getAbsolutePathOfTestsFolder());
-		BotIssue issue = new BotIssue();
-		issue.setFilePath(fileWithCodeSmell.getName());
-		issue.setLine(lineNumberOfMethodToBeRenamed);
-		issue.setJavaRoots(javaRoots);
-		issue.setRefactorString(newMethodName);
-		issue.setAllJavaFiles(Arrays.asList(fileWithCodeSmell.getCanonicalPath(), fileOfSuperClass.getCanonicalPath(),
-				fileOfSubClass.getCanonicalPath(), fileWithCallerMethod.getCanonicalPath(),
-				fileOfSiblingClass.getCanonicalPath()));
+						cuOriginalFileOfTestClass);
 
 		// act
-		RenameMethod refactoring = new RenameMethod();
-		String outputMessage = refactoring.performRefactoring(issue, gitConfig);
-		logger.info(outputMessage);
+		performRenameMethod(filesToConsider, fileOfTestClass, lineNumberOfMethodToBeRenamed, newMethodName);
 
 		// assert
-		CompilationUnit cuRefactoredFileWithCodeSmell = JavaParser.parse(fileWithCodeSmell);
-		CompilationUnit cuRefactoredFileWithCallerMethod = JavaParser.parse(fileWithCallerMethod);
-		CompilationUnit cuRefactoredFileOfSuperClass = JavaParser.parse(fileOfSuperClass);
-		CompilationUnit cuRefactoredFileOfSubClass = JavaParser.parse(fileOfSubClass);
-		CompilationUnit cuRefactoredFileOfSiblingClass = JavaParser.parse(fileOfSiblingClass);
+		CompilationUnit cuRefactoredFileOfTestClass = JavaParser.parse(fileOfTestClass);
 
 		// assert that target method has been renamed
-		assertThatMethodNameIsEqualToExpected(cuRefactoredFileWithCodeSmell, lineNumberOfMethodToBeRenamed,
+		assertThatMethodNameIsEqualToExpected(cuRefactoredFileOfTestClass, lineNumberOfMethodToBeRenamed,
 				newMethodName);
 
 		// assert that dummy method with same name is unchanged
 		int lineNumberOfDummyMethod = renameMethodTestClass.getLineOfMethodToBeRenamed();
-		assertThatMethodNameIsEqualToExpected(cuRefactoredFileWithCodeSmell, lineNumberOfDummyMethod,
+		assertThatMethodNameIsEqualToExpected(cuRefactoredFileOfTestClass, lineNumberOfDummyMethod,
 				originalDummyMethod.getNameAsString());
 
 		// assert that inner class method with same name as target method is unchanged
 		int lineNumberOfMethodWithTargetMethodSignatureInInnerClass = renameMethodInnerTestClass
 				.getLineOfMethodToBeRenamed(true);
-		assertThatMethodNameIsEqualToExpected(cuRefactoredFileWithCodeSmell,
+		assertThatMethodNameIsEqualToExpected(cuRefactoredFileOfTestClass,
 				lineNumberOfMethodWithTargetMethodSignatureInInnerClass,
 				originalMethodWithTargetMethodSignatureInInnerClass.getNameAsString());
 
 		// assert that caller method in same file has been refactored
 		int lineNumberOfCallerMethod = renameMethodTestClass.getLineOfMethodThatCallsMethodToBeRenamed();
-		assertThatNumberOfMethodCallsIsEqualToExpected(cuRefactoredFileWithCodeSmell, lineNumberOfCallerMethod,
+		assertThatNumberOfMethodCallsIsEqualToExpected(cuRefactoredFileOfTestClass, lineNumberOfCallerMethod,
 				newMethodName, 1);
+	}
+
+	@Test
+	public void testCallerClassRefactored() throws Exception {
+		// arrange
+		List<File> filesToConsider = new ArrayList<File>();
+		filesToConsider.add(fileOfTestClass);
+		filesToConsider.add(fileWithCallerMethod);
+		int lineNumberOfMethodToBeRenamed = renameMethodTestClass.getLineOfMethodToBeRenamed(true);
+		String newMethodName = "newMethodName";
+
+		// act
+		performRenameMethod(filesToConsider, fileOfTestClass, lineNumberOfMethodToBeRenamed, newMethodName);
 
 		// assert that caller method in different file has been refactored
+		CompilationUnit cuRefactoredFileWithCallerMethod = JavaParser.parse(fileWithCallerMethod);
 		int lineNumberOfCallerInDifferentFile = renameMethodCallerTestClass.getLineOfCallerMethodInDifferentFile();
 		assertThatNumberOfMethodCallsIsEqualToExpected(cuRefactoredFileWithCallerMethod,
 				lineNumberOfCallerInDifferentFile, newMethodName, 1);
+	}
+
+	@Test
+	public void testSuperClassRefactored() throws Exception {
+		// arrange
+		List<File> filesToConsider = new ArrayList<File>();
+		filesToConsider.add(fileOfTestClass);
+		filesToConsider.add(fileOfSuperClass);
+		int lineNumberOfMethodToBeRenamed = renameMethodTestClass.getLineOfMethodToBeRenamed(true);
+		String newMethodName = "newMethodName";
+
+		// act
+		performRenameMethod(filesToConsider, fileOfTestClass, lineNumberOfMethodToBeRenamed, newMethodName);
 
 		// assert that target's super class has been refactored
+		CompilationUnit cuRefactoredFileOfSuperClass = JavaParser.parse(fileOfSuperClass);
 		int lineNumberOfMethodInSuperClass = renameMethodSuperClass.getLineOfMethodToBeRenamed(true);
 		assertThatMethodNameIsEqualToExpected(cuRefactoredFileOfSuperClass, lineNumberOfMethodInSuperClass,
 				newMethodName);
+	}
+
+	@Test
+	public void testSubClassRefactored() throws Exception {
+		// arrange
+		List<File> filesToConsider = new ArrayList<File>();
+		filesToConsider.add(fileOfTestClass);
+		filesToConsider.add(fileOfSubClass);
+		int lineNumberOfMethodToBeRenamed = renameMethodTestClass.getLineOfMethodToBeRenamed(true);
+		String newMethodName = "newMethodName";
+
+		// act
+		performRenameMethod(filesToConsider, fileOfTestClass, lineNumberOfMethodToBeRenamed, newMethodName);
 
 		// assert that target's sub class has been refactored
+		CompilationUnit cuRefactoredFileOfSubClass = JavaParser.parse(fileOfSubClass);
 		int lineNumberOfMethodInSubClass = renameMethodSubClass.getLineOfMethodToBeRenamed(true);
 		assertThatMethodNameIsEqualToExpected(cuRefactoredFileOfSubClass, lineNumberOfMethodInSubClass, newMethodName);
+	}
+
+	@Test
+	public void testSiblingClassRefactored() throws Exception {
+		// arrange
+		List<File> filesToConsider = new ArrayList<File>();
+		filesToConsider.add(fileOfTestClass);
+		filesToConsider.add(fileOfSiblingClass);
+		int lineNumberOfMethodToBeRenamed = renameMethodTestClass.getLineOfMethodToBeRenamed(true);
+		String newMethodName = "newMethodName";
+
+		// act
+		performRenameMethod(filesToConsider, fileOfTestClass, lineNumberOfMethodToBeRenamed, newMethodName);
+
+		// assert
+		CompilationUnit cuRefactoredFileOfSiblingClass = JavaParser.parse(fileOfSiblingClass);
 
 		// assert that target's sibling has been refactored
 		int lineNumberOfMethodInSiblingClass = renameMethodSiblingClass.getLineOfMethodToBeRenamed(true);
@@ -184,6 +185,57 @@ public class RenameMethodTest extends AbstractRefactoringTests {
 				lineNumberOfCallerMethodInSiblingClass, newMethodName, 1);
 	}
 
+	@Test
+	public void testRenameMethodUnchangedMethodName() throws Exception {
+		exception.expect(BotRefactoringException.class);
+
+		// arrange
+		List<File> filesToConsider = new ArrayList<File>();
+		filesToConsider.add(fileOfTestClass);
+		int lineNumberOfMethodToBeRenamed = renameMethodTestClass.getLineOfMethodToBeRenamed(true);
+		String newMethodName = "getLineOfMethodToBeRenamed";
+
+		// act
+		performRenameMethod(filesToConsider, fileOfTestClass, lineNumberOfMethodToBeRenamed, newMethodName);
+	}
+
+	/**
+	 * Tries to rename the method in the given file and line to the given new method name.
+	 * 
+	 * @param filesToConsider All files that make up the repository for the specific test
+	 * @param targetFile
+	 * @param lineNumberOfMethodToBeRenamed
+	 * @param newMethodName
+	 * @throws Exception
+	 */
+	private void performRenameMethod(List<File> filesToConsider, File targetFile, int lineNumberOfMethodToBeRenamed,
+			String newMethodName) throws Exception {
+		GitConfiguration gitConfig = new GitConfiguration();
+		gitConfig.setRepoFolder(getAbsolutePathOfTempFolder());
+
+		BotIssue issue = new BotIssue();
+		ArrayList<String> javaRoots = new ArrayList<>();
+		javaRoots.add(getAbsolutePathOfTestsFolder());
+		issue.setFilePath(targetFile.getName());
+		issue.setLine(lineNumberOfMethodToBeRenamed);
+		issue.setJavaRoots(javaRoots);
+		issue.setRefactorString(newMethodName);
+		List<String> allJavaFiles = new ArrayList<>();
+		for (File f : filesToConsider) {
+			allJavaFiles.add(f.getCanonicalPath());
+		}
+		issue.setAllJavaFiles(allJavaFiles);
+
+		RenameMethod refactoring = new RenameMethod();
+		String outputMessage = refactoring.performRefactoring(issue, gitConfig);
+		logger.info(outputMessage);
+	}
+	
+	/**
+	 * @param cu
+	 * @param lineNumberOfMethodUnderTest
+	 * @param expectedMethodName
+	 */
 	private void assertThatMethodNameIsEqualToExpected(CompilationUnit cu, int lineNumberOfMethodUnderTest,
 			String expectedMethodName) {
 		MethodDeclaration methodUnderTest = RefactoringHelper
@@ -192,6 +244,12 @@ public class RenameMethodTest extends AbstractRefactoringTests {
 		assertThat(methodUnderTest.getNameAsString()).isEqualTo(expectedMethodName);
 	}
 
+	/**
+	 * @param cu
+	 * @param lineNumberOfMethodUnderTest
+	 * @param calledMethodName
+	 * @param expectedNumberOfMethodCalls
+	 */
 	private void assertThatNumberOfMethodCallsIsEqualToExpected(CompilationUnit cu, int lineNumberOfMethodUnderTest,
 			String calledMethodName, int expectedNumberOfMethodCalls) {
 		MethodDeclaration methodInSiblingClassWithSiblingMethodCall = RefactoringHelper
