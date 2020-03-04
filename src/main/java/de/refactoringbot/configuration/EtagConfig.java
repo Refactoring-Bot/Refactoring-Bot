1f8f4b975c4c48446ce9dc78f1204344b785c0f9
D-Dragon <dxdragon95@gmail.com>
2020-03-04 06:29:23 +0100

    Added Etag to Rest calls
    Returns 304 Status if data from call hasn't changed and loads it from cache

---
+++ src/main/java/de/refactoringbot/configuration/EtagConfig.java
@@ -0,0 +1,21 @@
+package de.refactoringbot.configuration;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+//@EnableWebMvc
+@Configuration
+public class EtagConfig {
+
+    @Bean
+    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
+        FilterRegistrationBean<ShallowEtagHeaderFilter> filterRegistrationBean
+          = new FilterRegistrationBean<>( new ShallowEtagHeaderFilter());
+        filterRegistrationBean.addUrlPatterns("/git-users/*","/configurations/*");
+        filterRegistrationBean.setName("etagFilter");
+        return filterRegistrationBean;
+    }
+}
